fcc9d7dd1dca14f053190d8b392624a06fa8ee3b
QWJ <wenjingQ555@outlook.com>
2023-01-07 01:50:25 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+mod ascii;
+mod debug;
+mod player;
+mod tilemap;
+
+use ascii::AsciiPlugin;
+use debug::DebugPlugin;
+use player::PlayerPlugin;
+use tilemap::TileMapPlugin;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
+
+fn main() {
+    let height = 900.0;
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height,
+            title: "Bevy Tutorial".to_string(),
+            vsync: true,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(AsciiPlugin)
+        .add_plugin(DebugPlugin)
+        .add_plugin(TileMapPlugin)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
